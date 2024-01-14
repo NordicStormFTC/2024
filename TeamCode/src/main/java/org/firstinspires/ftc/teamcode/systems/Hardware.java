c86066ac72614a7ebc2cac2fb5a82a1a3304110f
AlexWBur <abur@stpeterSchools.org>
2024-01-13 21:30:08 -0600

    Basic package class outline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Hardware.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.systems;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 public class Hardware {
 
+    public static Gamepad ethan = new Gamepad();
+    public static Gamepad mica = new Gamepad();
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Updates.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Updates.java
@@ -2,4 +2,12 @@ package org.firstinspires.ftc.teamcode.systems;
 
 public class Updates {
 
+    public static void updateAll(){
+
+    }
+
+    public static void updateArm(){
+
+    }
+
 }
