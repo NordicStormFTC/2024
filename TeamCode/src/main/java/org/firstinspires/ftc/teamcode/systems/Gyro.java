45d09025ed108c06b82fc17300498a580635aa83
alex <abur@stpeterschools.org>
2024-08-13 18:49:02 -0500

    fixed OpenCvProcessor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Gyro.java
@@ -1,20 +1,38 @@
 package org.firstinspires.ftc.teamcode.systems;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
 public class Gyro {
 
+    double adjustedAngle =0;
+    public BNO055IMU gyro;
     public Gyro(){
 
     }
 
     public void initialize(){
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+        parameters.loggingTag = "IMU";
+        gyro.initialize(parameters);
 
     }
 
-    public void getAngle(){
-
+    public double getAngle(){
+        Orientation o = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        return  o.firstAngle - adjustedAngle;
     }
 
     public void resetAngle(){
-
+        double angleDifference = getAngle() + adjustedAngle;
+        
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/vision/AprilTagProcessor.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.systems.vision;
-
-public class AprilTagProcessor {
-
-}
